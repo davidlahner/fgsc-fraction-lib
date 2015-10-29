b71c6a876485bade4cf9fccb2b94b98ad9c16074
eggerschwiler <mister_x4@hotmail.com>
2015-10-29 10:12:06 +0100

    SmokeTest

---
+++ fgsc-fraction-library/test/ch/zuehlke/scfg/fraction/SmokeTest.java
@@ -0,0 +1,14 @@
+package ch.zuehlke.scfg.fraction;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SmokeTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
